064086d63cfaf5076811f85120fda42d1752ba8a
Mark Comia <itzextinct2@gmail.com>
2025-05-25 21:06:58 +0800

    fix indentation errors

--- quiz_game_oop/quiz_game_oop_code.py
+++ quiz_game_oop/quiz_game_oop_code.py
@@ -22,65 +22,67 @@ class QuizPlayer(QuizBase):
             lines = file.readlines()
 
 #formatting of the questions/choices
-questions = []
-i = 0
-while i < len(lines):
-   if lines[i].startswith('#QUESTIONS'):
-      q = lines[i + 1].strip()
-      a = lines[i + 3].strip()
-      b = lines[i + 4].strip()
-      c = lines[i + 5].strip()
-      d = lines[i + 6].strip()
-      correct = lines[i + 7].strip()[-1]
-      questions.append((q,a,b,c,d,correct))
-      i += 9
-   else:
-      i += 1
-#choosing random questions
-random.shuffle(questions)
 
-#tracking of scores
-score = 0
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith('#QUESTIONS'):
+            q = lines[i + 1].strip()
+            a = lines[i + 3].strip()
+            b = lines[i + 4].strip()
+            c = lines[i + 5].strip()
+            d = lines[i + 6].strip()
+            correct = lines[i + 7].strip()[-1]
+            questions.append((q,a,b,c,d,correct))
+            i += 9
+        else:
+            i += 1
 
-#printing the question and choices
-for q in questions:
-   print('\n' + q[0])
-   print(q[1])
-   print(q[2])
-   print(q[3])
-   print(q[4])
+    def play(self):
+        self.greet()
+        self.load_questions()
+        random.shuffle(questions)               #choosing random questions
+
+        for q in questions:
+            print('\n' + q[0])
+            print(q[1])
+            print(q[2])
+            print(q[3])
+            print(q[4])
 
 #checks if answer is right
-   ans = input('What\'s your answer? (a/b/c/d) or "x" to exit: ').lower()
-   if ans == 'x':
-         print('Exiting program....Byeeee!')
-         time.sleep(2)
-         break
+            ans = input('What\'s your answer? (a/b/c/d) or "x" to exit: ').lower()
+            if ans == 'x':
+                print('Exiting program....Byeeee!')
+                time.sleep(2)
+                break
    
-   while ans not in ['a','b','c','d',"x"]:
-      print('Invalid answer,please refer to the choices')
-      ans = input('What\'s your answer? (a/b/c/d or "x" to exit):'.lower())
+            while ans not in ['a','b','c','d',"x"]:
+                print('Invalid answer,please refer to the choices')
+                ans = input('What\'s your answer? (a/b/c/d or "x" to exit):'.lower())
 
-      if ans == 'x':
-         print('Exiting program....Byeeee!')
-         time.sleep(2)
-         break
-   if ans == 'x':
-      break
+                if ans == 'x':
+                    print('Exiting program....Byeeee!')
+                    time.sleep(2)
+                    break
+            if ans == 'x':
+                break
 
-   if ans == q[5]:
-      print('Checking answer...')
-      time.sleep(1)
-      print('✅ Correct!')
-      winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
-      score += 1
-   else:
-      print('Checking answer...')
-      time.sleep(1)
-      print(f'❌ Wrong! The correct answer is {q[5]}')
-      winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
-      
-#Final Score
+        if ans == q[5]:
+            print('Checking answer...')
+            time.sleep(1)
+            print('✅ Correct!')
+            winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
+            score += 1
+        else:
+            print('Checking answer...')
+            time.sleep(1)
+            print(f'❌ Wrong! The correct answer is {q[5]}')
+            winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
+
+    self.show_score()
+
+def show_score(self):
+    total = len(self.questions)
 if score == 0:
    print(f'Wala Kang Tinama Kahit Isa, Mag Review Ka Pa! {score} out of {len(questions)}')
 elif score == len(questions):
